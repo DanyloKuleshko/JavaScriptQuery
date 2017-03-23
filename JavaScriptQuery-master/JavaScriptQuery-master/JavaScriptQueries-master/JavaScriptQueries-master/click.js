5c9456ba8cebaabd49e53bc964e93b2bd2ceb6b1
DanyloKuleshko <7dkuleshko@stpatrick.org>
2017-03-23 10:48:46 -0500

    Add files via upload

---
+++ JavaScriptQuery-master/JavaScriptQuery-master/JavaScriptQueries-master/JavaScriptQueries-master/click.js
@@ -0,0 +1,14 @@
+//declare samecolor() here
+function samecolor(){
+  const doc = document;
+  let color ="#5f42f4";
+  let divs = doc.querySelectorAll("#samecolor dic");
+
+  divs[0].style.backgroundColor = color;
+  divs[0].style.height = "700px"
+  divs[1].style.opacity = 0.86;
+}
+//declare diffcolor() here
+function diffcolor(){
+  let color = "#cb42f4"
+}
